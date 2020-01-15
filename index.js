454c8e58dd14a83c2210ffdc16ac49819f2bf7cd
KleinosFR <kleinos@hotmail.com>
2020-01-15 16:36:40 +0100

    general chat working

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 8000;
+const server = require("http").Server(app);
+const socketIO = require("socket.io");
 
 // Get the Sequelize config
 const sequelize = require("./sequelize");
@@ -16,6 +18,55 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("Hello world !"));
 app.use("/chatMessages", require("./routes/chatmessage.routes"));
 
+// Get the websocket manager
+const io = socketIO(server);
+// Websocket
+let clients = [];
+
+io.sockets.on("connection", socket => {
+  clients.push(socket);
+  console.log("Connected : sockets connected : ", clients.length);
+
+  // Disconnect
+  socket.on("disconnect", socket => {
+    clients.splice(clients.indexOf(socket), 1);
+    console.log("Disconnected : sockets conected : ", clients.length);
+  });
+
+  io.clients((error, clients) => {
+    if (error) throw error;
+    console.log(clients); // => [6em3d4TJP8Et9EMNAAAA, G5p55dHhGgUnLUctAAAB]
+  });
+  socket.emit("message", { message: "connected to chat", user: "server" });
+  socket.on("message", data => {
+    console.log(data);
+    socket.broadcast.emit("message", data);
+    socket.on("disconnect", () => {
+      console.log(`Socket ${socket.id} disconnected.`);
+    });
+  });
+  socket.on("subscribe", function(room) {
+    console.log("joining room", room);
+    socket.join(room);
+  });
+  socket.on("unsubscribe", function(room) {
+    console.log("leaving room", room);
+    socket.leave(room);
+  });
+  socket.on("send", function(data) {
+    console.log("sending room post", data.room);
+    socket.broadcast.to(data.room).emit("message", {
+      message: data.message,
+      user: data.user
+    });
+  });
+});
+
+// ********* List all the clients conected
+
+// ********* private chat
+
+// check if test environment
 if (process.env.NODE_ENV === "test") {
   app.use("/token", require("./routes/tokenForTests.routes"));
 }
@@ -24,7 +75,7 @@ async function main() {
   try {
     await sequelize.sync(); // Sync Method will create Database using the config & models
     console.log("Database connection sucessfull");
-    app.listen(PORT, err => {
+    server.listen(PORT, err => {
       if (err) throw new Error("Something bad happened...");
       console.log(`Listening to port ${PORT}.`);
     });
